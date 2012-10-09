b200848616613fd11397512bfe917ae7db90e534
Pavit <pavitm@gmail.com>
2012-10-08 23:34:13 -0400

    new app

--- pollproject/urls.py
+++ pollproject/urls.py
@@ -1,17 +1,24 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic.simple import direct_to_template
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from	django.contrib	import admin
+#from facebook.templatetags import facebook_tags
+admin.autodiscover()
+import django.contrib.auth
+import core
+from core import views
+from django.conf import settings
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pollproject.views.home', name='home'),
-    # url(r'^pollproject/', include('pollproject.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+urlpatterns	=	patterns('',
+	url(r'^admin/doc/',	include('django.contrib.admindocs.urls')),
+	url(r'^poll/', include('poll.urls')),
+	url(r'^admin/',	include(admin.site.urls)),
+	url(r'^$', core.views.landing, name='landing'),
+	url(r'^done/$', core.views.done, name='done'),
+	url(r'^profile/$', core.views.profile, name = 'profile'),
+	url(r'', include('social_auth.urls')),
+	url(r'^logout/$', core.views.logout, name='logout'),
+	(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+	url(r'^admin/', include(admin.site.urls)),
+	(r'^tagging_autocomplete_tagit/', include('tagging_autocomplete_tagit.urls')),
 )
